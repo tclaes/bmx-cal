65d16684a540bb6ea3d235193b43aab382d491bf
Tom Claes <tom.claes82@gmail.com>
2026-03-23 22:34:40 +0100

    Updated version.ts

--- src/shared/config/version.ts
+++ src/shared/config/version.ts
@@ -1,4 +1,4 @@
 export const APP_VERSION = '1.0.0';
 export const MIN_VERSION = '1.0.0';
-export const FORCE_UPDATE = false;
+export const FORCE_UPDATE = true;
 export const RELEASE_NOTES = 'Initial stable release with PWA support';
