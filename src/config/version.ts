6db8765e9d86f2b90bea236d0e52b1483075b18c
Tom Claes <tom.claes82@gmail.com>
2026-04-30 22:43:17 +0200

    Updated version.ts

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.0.6';
+export const APP_VERSION = '0.0.7';
 export const MIN_VERSION = '0.0.6';
 export const FORCE_UPDATE = false;
-export const RELEASE_NOTES = 'Initial stable release with PWA support';
+export const RELEASE_NOTES = 'Require account for personal calendar creation';
